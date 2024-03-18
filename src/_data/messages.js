a6ac63b40bdf8d7631134922170a8e311f151a09
Bruce Taylor <brootaylor@gmail.com>
2024-03-18 12:51:10 +0000

    Message formatting fixes

--- src/_data/messages.js
+++ src/_data/messages.js
@@ -1,7 +1,7 @@
 module.exports = {
   info: {
     modifier: 'message--info',
-    message: 'I am currently on the lookout for a new opportunity. If you think I might make a good addition to your team,',
+    message: 'I am currently on the lookout for a new opportunity. If you think I might make a good addition to your team, ',
     ctaUrl: '/contact',
     ctaLabel: 'please get in touch',
   },
